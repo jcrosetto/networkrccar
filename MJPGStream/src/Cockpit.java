29f926ba4af643ddc7a9addeaf583b8d44f22a58
thelatterrain <thelatterrain@abced81e-8520-11dd-ad27-0f66e57d2035>
2009-04-07 00:10:13 +0000

    small change adding constructor to Cockpit class

--- MJPGStream/src/Cockpit.java
+++ MJPGStream/src/Cockpit.java
@@ -2,15 +2,21 @@ import javax.swing.JFrame;
 
 
 public class Cockpit {
-
-	public static void main(String[] args) {
-		JFrame jframe = new JFrame();
+	
+	private JFrame jframe = new JFrame();
+	private AxisCamera axPanel = new AxisCamera();
+	
+	public Cockpit(){
 		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		AxisCamera axPanel = new AxisCamera();
 		new Thread(axPanel).start();
 		jframe.getContentPane().add(axPanel);
 		jframe.setSize(320, 240);
 		jframe.setTitle("Big Dog Cockpit");
 		jframe.setVisible(true);
+	}
+
+
+	public static void main(String[] args) {
+		Cockpit show = new Cockpit();
 	}
 }
