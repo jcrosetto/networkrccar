0e0acff6c61aba0e6c6b182b971eaf2b99544313
thelatterrain <thelatterrain@abced81e-8520-11dd-ad27-0f66e57d2035>
2009-04-15 02:34:03 +0000

    almost working. just need to figure out why '12' is being read in as '1', '2'

--- Server/server.c
+++ Server/server.c
@@ -12,15 +12,48 @@
 #include <string.h>
 #include <unistd.h>
 #include <netinet/in.h>
+//these are for the alarm output
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <asm/arch/gpiodriver.h>
 
 #define MAXPENDING 5    /* Max connection requests */
 #define BUFFSIZE 32
+
+int fda;
+int inta = 1;
+int l = 0;
+int a = 1 << 3;
+
 void Crash(char *err) { perror(err); exit(1); }
 
+/**
+ * turn alarm on and off specified amount of times
+ */
+void pulse(int state){
+	int i;
+	printf("COMMAND RECEIVED: %d\n", state);
+	for(i = 0; i<state; i++){
+		printf("%d - ",i);
+		ioctl(fda, _IO(GPIO_IOCTYPE, IO_SETBITS), a);
+		printf("%s","on_");
+		ioctl(fda, _IO(GPIO_IOCTYPE, IO_CLRBITS), a);
+		printf("%s\n","off");
+	}
+	//after the right amount of pulses have been sent
+	//sleep for 10 ms for microprocessor to know end of signal
+	printf("%s","END_");
+	ioctl(fda, _IO(GPIO_IOCTYPE, IO_SETBITS), a);
+	printf("%s","on_");
+	usleep(1);
+	ioctl(fda, _IO(GPIO_IOCTYPE, IO_CLRBITS), a);
+	printf("%s","off\n");
+}
 
 void HandleClient(int sock) {
 	char buffer[BUFFSIZE];
 	int received = -1;
+	int state = 0;
 
 	/* Receive message */
 	if ((received = recv(sock, buffer, BUFFSIZE, 0)) < 0) {
@@ -29,28 +62,35 @@ void HandleClient(int sock) {
 
 	/* Send bytes and check for more incoming data in loop */
 	while (received > 0) {
-		//testing println
-		buffer[received] = '\n';
-		printf("received: %s\n", buffer);
+		//printf("received: %s", buffer);
+
+		state = atoi(buffer);
+		pulse(state);
+		state = 0;
+
+		buffer[received]='\n';
+		//try this? buffer[received]='\0';
+
 		/* Send back received data */
 		if (send(sock, buffer, received, 0) != received) {
 			Crash("Failed to send bytes to client");
 		}
-
-		printf("called send\n");
-
 		/* Check for more data */
 		if ((received = recv(sock, buffer, BUFFSIZE, 0)) < 0) {
 			Crash("Failed to receive additional bytes from client");
 		}//end if
 	}//end while
-	printf("closing socket\n");
 	close(sock);
 }//end HandleClient
+
 
 int main(int argc, char *argv[]) {
 	int serversock, clientsock;
 	struct sockaddr_in echoserver, echoclient;
+
+	//file for writing
+	fda = open("/dev/gpioa", O_RDWR);
+	//ioctl(fda, _IO(GPIO_IOCTYPE, IO_SETGET_OUTPUT), &inta);
 
 	if (argc != 2) {
 		fprintf(stderr, "USAGE: server.o <port>\n");
