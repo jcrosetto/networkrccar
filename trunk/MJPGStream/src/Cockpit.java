ffca6d7e25dbe07b2c8c401b6188f05af8f475b6
dreamtennis <dreamtennis@abced81e-8520-11dd-ad27-0f66e57d2035>
2009-05-01 01:01:56 +0000

    added controller stuff

--- trunk/MJPGStream/src/Cockpit.java
+++ trunk/MJPGStream/src/Cockpit.java
@@ -4,6 +4,7 @@ import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 
+import net.java.games.input.*;
 import javax.swing.OverlayLayout;
 import java.awt.event.*;
 import java.io.BufferedReader;
@@ -39,56 +40,58 @@ public class Cockpit implements KeyListener, ActionListener {
 			"GUIspeed2.png", "GUIspeed3.png", "GUIspeed4.png", "GUIspeed5.png" };
 	final private String[] ARROWPATHS = { "GUILeftOff.png", "GUILeftOn.png",
 			"GUIRightOff.png", "GUIRightOn.png" };
-	final private String[] LOGOPATHS = {"GUIlogoOff.png" , "GUIlogoOn.png"};
+	final private String[] LOGOPATHS = { "GUIlogoOff.png", "GUIlogoOn.png" };
 
 	private int speed = 0;
 	private int key;
 	private int turnKeyPressed = 0; // if turn key pressed wait for release
 	private int state = 0; // really 0 but 0000 to show that figuratively we are
-							// using
+	// using
 	// state represented by a 4-bit binary number
 	private char lastChar;
 	private boolean isConnected = false;
 
-	//main containers
+	// main containers
 	private JFrame frame = new JFrame("Big Dog Cockpit");
-	private JPanel view = new JPanel(); //first tabbed pane
-	private JPanel setup = new JPanel(); //second tabbed pane
-	
-	//sub-containers
+	private JPanel view = new JPanel(); // first tabbed pane
+	private JPanel setup = new JPanel(); // second tabbed pane
+
+	// sub-containers
 	private JPanel indicatorPane = new JPanel(); //
 	private JPanel feedPane = new JPanel();
 	private JPanel controlPane = new JPanel();
 	private JPanel graphics = new JPanel();
-	
-	//textfields to go in the "Setup" panel
+
+	// textfields to go in the "Setup" panel
 	private JTextField hostnameField, mjpgStreamField, userField, passField;
-	private JTextField portField = new JTextField(Integer.toString(port), 4); // refers to port server
-																			  // is listening on
-	//two buttons needed
+	private JTextField portField = new JTextField(Integer.toString(port), 4); // refers
+																				// to
+																				// port
+																				// server
+	// is listening on
+	// two buttons needed
 	private JButton button1 = new JButton("Connect");
 	private JButton button2 = new JButton("Update");
-	
-	//icon images are loaded in here and toggled based on state
+
+	// icon images are loaded in here and toggled based on state
 	private ImageIcon[] speeds;
 	private ImageIcon[] directions;
 	private ImageIcon[] logos;
-	
-	//JLabels used to hold icons
+
+	// JLabels used to hold icons
 	private JLabel speedGauge = new JLabel();
 	private JLabel leftInd = new JLabel();
 	private JLabel rightInd = new JLabel();
 	private JLabel logo = new JLabel();
 	private AxisCamera axPanel = new AxisCamera();
 
-	//communication with car's server necessities
+	// communication with car's server necessities
 	Socket controller;
 	BufferedReader input;
 	DataOutputStream output;
 
 	/**
-	 *** Constructor *** 
-	 * set up jframe then open socket connection
+	 *** Constructor *** set up jframe then open socket connection
 	 * 
 	 * @param host
 	 * @param port
@@ -99,12 +102,12 @@ public class Cockpit implements KeyListener, ActionListener {
 		mjpgStreamField = new JTextField(axPanel.mjpegStream, 24);
 		userField = new JTextField(axPanel.user, 5);
 		passField = new JTextField(axPanel.pass, 5);
-		
-		//grab all of the indicatorPane images
+
+		// grab all of the indicatorPane images
 		speeds = createImageIcon(SPEEDPATHS, "speed");
 		directions = createImageIcon(ARROWPATHS, "direction");
 		logos = createImageIcon(LOGOPATHS, "logo");
-		
+
 		if (speeds != null && directions != null && logos != null) {
 			speedGauge.setIcon(speeds[0]);
 			leftInd.setIcon(directions[0]);
@@ -120,7 +123,7 @@ public class Cockpit implements KeyListener, ActionListener {
 		addComponentsToPane(frame.getContentPane());
 		frame.setBackground(new Color(0xA7A7A7));
 		frame.setVisible(true);
-		//frame.setResizable(false);
+		// frame.setResizable(false);
 
 		// open socket and io streams
 		// openSocket();
@@ -129,30 +132,32 @@ public class Cockpit implements KeyListener, ActionListener {
 
 	/**
 	 *** Set up both of the tabbed panes and add all the components to it ***
+	 * 
 	 * @param pane
 	 */
 	private void addComponentsToPane(Container pane) {
-		
-		//holds the view and setup tabs using car layout
+
+		// holds the view and setup tabs using car layout
 		JTabbedPane tabbedPane = new JTabbedPane();
 		tabbedPane.setName("Big Dog Cockpit");
-		
-		//labels
+
+		// labels
 		JLabel hst = new JLabel("hostname or IP address");
 		JLabel prt = new JLabel("port number");
 		JLabel mjpg = new JLabel("MJPEG Stream Source");
 		JLabel usr = new JLabel("Username");
 		JLabel pwd = new JLabel("Password");
-		
-		//add action descriptors
+
+		// add action descriptors
 		button1.setActionCommand("toggleConnect");
 		button2.setActionCommand("update");
-		
-		//set appropriate layouts for all the panes
+
+		// set appropriate layouts for all the panes
 		pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
 		view.setLayout(new BoxLayout(view, BoxLayout.Y_AXIS));
 		setup.setLayout(new FlowLayout(FlowLayout.LEFT));
-		indicatorPane.setLayout(new BoxLayout(indicatorPane, BoxLayout.LINE_AXIS));
+		indicatorPane.setLayout(new BoxLayout(indicatorPane,
+				BoxLayout.LINE_AXIS));
 		graphics.setLayout(new BoxLayout(graphics, BoxLayout.Y_AXIS));
 
 		// view.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -163,43 +168,45 @@ public class Cockpit implements KeyListener, ActionListener {
 		new Thread(axPanel).start();
 
 		// setup indicatorPane
-		indicatorPane.add(Box.createRigidArea(new Dimension(15,0)));
+		indicatorPane.add(Box.createRigidArea(new Dimension(15, 0)));
 		indicatorPane.add(leftInd);
 		indicatorPane.add(Box.createHorizontalGlue());
 		indicatorPane.add(speedGauge);
 		indicatorPane.add(Box.createHorizontalGlue());
 		indicatorPane.add(rightInd);
-		indicatorPane.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
-		indicatorPane.add(Box.createRigidArea(new Dimension(15,0)));
+		indicatorPane.setBorder(BorderFactory
+				.createEtchedBorder(EtchedBorder.LOWERED));
+		indicatorPane.add(Box.createRigidArea(new Dimension(15, 0)));
 		indicatorPane.setBackground(new Color(0x222222));
-		
-		//setup feedPane
+
+		// setup feedPane
 		feedPane.add(axPanel);
-		feedPane.setPreferredSize(new Dimension(360,260));
+		feedPane.setPreferredSize(new Dimension(360, 260));
 		feedPane.setMaximumSize(new Dimension(340, 280));
 		feedPane.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
 		feedPane.setBackground(new Color(0x3B3B3B));
-		
-		//setup controlPane
+
+		// setup controlPane
 		controlPane.add(button1);
 		button1.setAlignmentX((float) .5);
-		controlPane.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
-		//controlPane.setOpaque(false);
+		controlPane.setBorder(BorderFactory
+				.createEtchedBorder(EtchedBorder.LOWERED));
+		// controlPane.setOpaque(false);
 		controlPane.setBackground(new Color(0x3B3B3B));
 		controlPane.setMaximumSize(new Dimension(340, 30));
-		
-		//setup graphics pane
+
+		// setup graphics pane
 		graphics.add(logo);
 		graphics.setBackground(new Color(0xFAFAFA));
 		graphics.setMaximumSize(new Dimension(340, 360));
 		logo.setAlignmentX((float) 0.5);
-		
+
 		// add live view, indicatorPane, and button to view tab
 		view.add(indicatorPane);
 		view.add(graphics);
-		view.add(Box.createRigidArea(new Dimension(0,3)));
+		view.add(Box.createRigidArea(new Dimension(0, 3)));
 		view.add(feedPane);
-		view.add(Box.createRigidArea(new Dimension(0,3)));
+		view.add(Box.createRigidArea(new Dimension(0, 3)));
 		view.add(controlPane);
 		view.add(Box.createVerticalGlue());
 		view.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED));
@@ -249,16 +256,18 @@ public class Cockpit implements KeyListener, ActionListener {
 	private boolean openSocket() {
 		boolean returnee = false;
 		try {
-			if(!axPanel.connected){
-				System.out.println("Re-Connect, connected: "+axPanel.connected);
+			if (!axPanel.connected) {
+				System.out.println("Re-Connect, connected: "
+						+ axPanel.connected);
 				axPanel = new AxisCamera();
-				//Thread.currentThread().sleep(9000);
-				//axPanel.run();
-				System.out.println("Re-Connect2, connected: "+axPanel.connected);
-				//System.out.println("Re-Connect, canceled: "+axPanel.parser.canceled);
+				// Thread.currentThread().sleep(9000);
+				// axPanel.run();
+				System.out.println("Re-Connect2, connected: "
+						+ axPanel.connected);
+				// System.out.println("Re-Connect, canceled: "+axPanel.parser.canceled);
 			}
-			
-			//axPanel = new AxisCamera();
+
+			// axPanel = new AxisCamera();
 			controller = new Socket(axPanel.hostName, port);
 			// Socket connection created
 			System.out.println("Connected to: " + axPanel.hostName
@@ -274,8 +283,7 @@ public class Cockpit implements KeyListener, ActionListener {
 		} catch (IOException e) {
 			System.out.println("Error opening socket: " + axPanel.hostName
 					+ " on port " + port + " - " + e);
-		}
-		catch(Exception ie){
+		} catch (Exception ie) {
 			System.out.println(ie);
 		}
 
@@ -293,7 +301,7 @@ public class Cockpit implements KeyListener, ActionListener {
 			input.close();
 			controller.close();
 			isConnected = false;
-			//axPanel.parser.canceled = true;
+			// axPanel.parser.canceled = true;
 			axPanel.disconnect();
 			returnee = true;
 			button1.setText("Connect");
@@ -303,7 +311,7 @@ public class Cockpit implements KeyListener, ActionListener {
 		} catch (IOException e) {
 			System.out.println(e);
 		}
-		
+
 		return returnee;
 	}
 
@@ -355,7 +363,7 @@ public class Cockpit implements KeyListener, ActionListener {
 				if (turnKeyPressed == 0) {
 					turnKeyPressed = key;
 					sendOut();
-					if(key == 37)
+					if (key == 37)
 						leftInd.setIcon(directions[1]);
 					else
 						rightInd.setIcon(directions[3]);
@@ -387,7 +395,7 @@ public class Cockpit implements KeyListener, ActionListener {
 				leftInd.setIcon(directions[0]);
 				rightInd.setIcon(directions[2]);
 			}
-		}//is Connected
+		}// is Connected
 	}
 
 	/**
@@ -501,7 +509,9 @@ public class Cockpit implements KeyListener, ActionListener {
 			if (isConnected) {
 				System.out.println("Closing connection...");
 				// axPanel.disconnect();
-				speed = 0; turnKeyPressed = 0; sendOut(); //send out a stopped state before disconnecting
+				speed = 0;
+				turnKeyPressed = 0;
+				sendOut(); // send out a stopped state before disconnecting
 				closeSocket();
 				speedGauge.setIcon(speeds[0]);
 				leftInd.setIcon(directions[0]);
@@ -527,4 +537,59 @@ public class Cockpit implements KeyListener, ActionListener {
 		}// if update button in 2nd pane
 	}
 
+	/*
+	 * controller
+	 */
+	public void controller(){
+		Controller[] ca = ControllerEnvironment.getDefaultEnvironment().getControllers();
+
+		Controller pad = null;
+        for(int i =0;i<ca.length;i++){
+
+            if(ca[i].getName() == "Logitech RumblePad 2 USB"){
+            	pad = ca[i];
+            	break;
+            }
+        }
+        
+        if(pad == null){
+        	System.out.println("Controller not found.");
+        	return;
+        }
+        	
+        Component[] components = pad.getComponents();
+        Rumbler[]  rumble = pad.getRumblers();
+        
+        while(true){
+        	
+        	pad.poll();
+        	EventQueue queue = pad.getEventQueue();
+        	
+        	Event event = new Event();
+        	
+        	while(queue.getNextEvent(event)) {
+				event.getNanos();
+				Component comp = event.getComponent();
+				comp.getName();
+				float value = event.getValue(); 
+				if(comp.isAnalog()) {
+					//value;
+				} else {
+					if(value==1.0f) {
+						//append("On");
+					} else {
+						//buffer.append("Off");
+					}
+				}
+				//System.out.println(buffer.toString());
+        	}
+		
+		try {
+			Thread.sleep(10);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        }
+	}
 }
